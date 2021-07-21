ff8b2a39d6dbff2d142908fcac4fd21a03b615da
kulabtech <46423311+kulabtech@users.noreply.github.com>
2021-07-21 15:24:19 +0530

    Update HelloController.java

--- src/main/java/com/techprimers/jenkins/cicdjenkinsexample/HelloController.java
+++ src/main/java/com/techprimers/jenkins/cicdjenkinsexample/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping{"/get"}
     public String hello(){
         return "My Application";
     }
